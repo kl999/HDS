2697f86ff6c3c8e3cb98da8db95110e16b95aaaf
kl <klstr999@gmail.com>
2025-07-29 19:21:20 +0500

    Add message id, acc

--- udp-connection/src/socket_worker.rs
+++ udp-connection/src/socket_worker.rs
@@ -1,15 +1,17 @@
 use std::{
-    collections::{HashMap, VecDeque}, net::UdpSocket, rc::Rc
+    collections::{HashMap, VecDeque},
+    net::UdpSocket,
+    rc::Rc,
 };
 
 use crate::message::Message;
 
-pub struct SocketWorker
-{
+pub struct SocketWorker {
     socket: UdpSocket,
     outgoing: VecDeque<Message>,
     incoming: HashMap<u64, Rc<Message>>,
     notify: fn(Rc<Message>),
+    message_id: u64,
 }
 
 impl SocketWorker {
@@ -19,6 +21,7 @@ impl SocketWorker {
             outgoing: VecDeque::with_capacity(1000),
             incoming: HashMap::new(),
             notify: f,
+            message_id: 1u64,
         }
     }
 
@@ -27,7 +30,9 @@ impl SocketWorker {
         self.send();
     }
 
-    pub fn send_message(&mut self, msg: Message) {
+    pub fn send_message(&mut self, msg: Box<[u8]>) {
+        let msg = Message::new(self.message_id, msg);
+        self.message_id += 1;
         self.outgoing.push_back(msg);
     }
 
@@ -44,7 +49,14 @@ impl SocketWorker {
                     msg
                 );
 
-                todo!("Check hash");
+                if !msg.check_hash() {
+                    return;
+                }
+
+                if msg.id == 0 {
+                    self.handle_ctrl(msg);
+                    return;
+                }
 
                 let msg = Rc::new(msg);
 
@@ -54,18 +66,36 @@ impl SocketWorker {
             Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
                 // No data is available right now
             }
-            Err(e) => { panic!("On receive {}", e) }
+            Err(e) => {
+                panic!("On receive {}", e)
+            }
         }
     }
-    
+
     fn send(&mut self) {
         if let Some(msg) = self.outgoing.front() {
             print!("Sending '{}'", msg);
             self.socket.send(&msg.serialize()).unwrap();
             let msg = self.outgoing.pop_front().expect("wtf?");
             self.outgoing.push_back(msg);
+        }
+    }
 
-            todo!("Delete received");
+    fn handle_ctrl(&mut self, msg: Message) {
+        match msg.data[0] {
+            1 => {
+                let id = u64::from_be_bytes(msg.data[1..9].try_into().expect("Error casting id!"));
+
+                let rem_ind = self
+                    .outgoing
+                    .iter()
+                    .position(|i| i.id == id)
+                    .expect("No outgoing to remove!");
+                self.outgoing.remove(rem_ind);
+            }
+            _ => {
+                panic!("Unknown message {}", msg.data[0]);
+            }
         }
     }
 }
